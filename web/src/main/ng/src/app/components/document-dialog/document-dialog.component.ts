6612b3cf47f86a7e071e75422c324e78719e9e26
alberto <alberto.hernandez@inovatika.cz>
2020-12-07 08:35:40 +0100

    #36

--- web/src/main/ng/src/app/components/document-dialog/document-dialog.component.ts
+++ web/src/main/ng/src/app/components/document-dialog/document-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AppService } from 'src/app/app.service';
 
 @Component({
   selector: 'app-document-dialog',
@@ -12,10 +13,18 @@ export class DocumentDialogComponent implements OnInit {
 
   constructor(
     public dialogRef: MatDialogRef<DocumentDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) { }
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private service: AppService) { }
 
   ngOnInit(): void {
     this.result = this.data;
+    this.getFullId();
+  }
+
+  getFullId() {
+    this.service.getId(this.result.ident_cely).subscribe((res: any) => {
+      this.result = res.response.docs[0];
+    });
   }
 
 }
