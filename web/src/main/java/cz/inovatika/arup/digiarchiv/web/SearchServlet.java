90b63bb409d2dcec86059b92ea0c5247e83fee3c
alberto <alberto.hernandez@inovatika.cz>
2020-12-09 15:56:52 +0100

    #49 Vyskovy bod

--- web/src/main/java/cz/inovatika/arup/digiarchiv/web/SearchServlet.java
+++ web/src/main/java/cz/inovatika/arup/digiarchiv/web/SearchServlet.java
@@ -110,7 +110,7 @@ public class SearchServlet extends HttpServlet {
                    .setFacet(false);
            query.setRequestHandler("/search");
           query.setFields("*,dok_jednotka:[json],pian:[json],adb:[json],jednotka_dokumentu:[json],nalez_dokumentu:[json],"
-                  + "ext_zdroj:[json],vazba_projekt_akce:[json],akce:[json],soubor:[json],let:[json],nalez:[json],"
+                  + "ext_zdroj:[json],vazba_projekt_akce:[json],akce:[json],soubor:[json],let:[json],nalez:[json],vyskovy_bod:[json],"
                   + "dokument:[json],projekt:[json],samostatny_nalez:[json],komponenta:[json],komponenta_dokument:[json],neident_akce:[json]");
           JSONObject jo = SearchUtils.json(query, client, "entities");
           if (jo.getJSONObject("response").optInt("numFound", 0) > 0) {
