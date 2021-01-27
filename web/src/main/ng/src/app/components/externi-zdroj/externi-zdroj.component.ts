73db42cbc9f61663d74d0564cbce1096ae486728
David Novák <novak@arup.cas.cz>
2021-01-27 15:52:15 +0100

    Update externi-zdroj.component.ts

--- web/src/main/ng/src/app/components/externi-zdroj/externi-zdroj.component.ts
+++ web/src/main/ng/src/app/components/externi-zdroj/externi-zdroj.component.ts
@@ -25,67 +25,64 @@ export class ExterniZdrojComponent implements OnInit {
     const autor = this.result.autori ? this.result.autori.split(';').join(' – ') : '';
     switch (this.result.typ) {
       case 'kniha':
-        this.bibTex = `@book{
-          https://digiarchiv.aiscr.cz/id/${this.result.ident_cely},
-          author = “${autor}”, 
-          title = “${this.result.nazev} ${this.result.podnazev ? this.result.podnazev : ''} ”,
-          ${this.result.vydavatel ? `publisher = “${this.result.vydavatel}”,` : ''}
-          year = ${this.result.rok_vydani_vzniku},
-          ${this.result.edice_rada ? `series = “${this.result.edice_rada}”,` : ''}
-          address = “${this.result.misto}”,
-          ${this.result.link ? `url = “${this.result.link}”,` : ''}
-          note = “ISBN: ${this.result.isbn}”
+        this.bibTex = `@book{https://digiarchiv.aiscr.cz/id/${this.result.ident_cely},
+          author = {${autor}}, 
+          title = {${this.result.nazev} ${this.result.podnazev ? this.result.podnazev : ''}},
+          ${this.result.vydavatel ? `publisher = {${this.result.vydavatel}},` : ''}
+          year = {${this.result.rok_vydani_vzniku}},
+          ${this.result.edice_rada ? `series = {${this.result.edice_rada}},` : ''}
+          ${this.result.link ? `url = {${this.result.link}},` : ''}
+          ${this.result.isbn ? `isbn = {${this.result.isbn}},` : ''}
+          ${this.result.issn ? `issn = {${this.result.issn}},` : ''}
+          address = {${this.result.misto}}
         }`;
         break;
       case 'část knihy':
-        this.bibTex = `@inproceedings{
-            https://digiarchiv.aiscr.cz/id/${this.result.ident_cely},
-            author = “${autor}”, 
-            title = “${this.result.nazev} ${this.result.podnazev ? this.result.podnazev : ''} ”,
-            booktitle = “${this.result.sbornik_nazev} ”,
-            year = ${this.result.rok_vydani_vzniku},
-            editor = ${this.result.sbornik_editor},
-            ${this.result.vydavatel ? `publisher = “${this.result.vydavatel}”,` : ''}
-            ${this.result.edice_rada ? `series = “${this.result.edice_rada}”,` : ''}
-            address = “${this.result.misto}”,
-            note = “ISSN: ${this.result.isbn}”,
-            ${this.result.link ? `url = “${this.result.link}”,` : ''}
-            pages = “${this.result.paginace_titulu}”
+        this.bibTex = `@inproceedings{https://digiarchiv.aiscr.cz/id/${this.result.ident_cely},
+            author = {${autor}}, 
+            title = {${this.result.nazev} ${this.result.podnazev ? this.result.podnazev : ''}},
+            booktitle = {${this.result.sbornik_nazev}},
+            year = {${this.result.rok_vydani_vzniku}},
+            editor = {${this.result.sbornik_editor}},
+            ${this.result.vydavatel ? `publisher = {${this.result.vydavatel}},` : ''}
+            ${this.result.edice_rada ? `series = {${this.result.edice_rada}},` : ''}
+            address = {${this.result.misto}},
+            ${this.result.issn ? `issn = {${this.result.issn}},` : ''}
+            ${this.result.isbn ? `isbn = {${this.result.isbn}},` : ''}
+            ${this.result.link ? `url = {${this.result.link}},` : ''}
+            pages = {${this.result.paginace_titulu}}
           }`;
         break;
       case 'článek v časopise':
-        this.bibTex = `@article{
-              https://digiarchiv.aiscr.cz/id/${this.result.ident_cely},
-              author = “${autor}”,
-              title = “${this.result.nazev} ${this.result.podnazev ? this.result.podnazev : ''} ”,
-              journal = “${this.result.casopis_denik_nazev} ”,
-              year = ${this.result.rok_vydani_vzniku},
-              volume = “${this.result.casopis_rocnik}”,
-              note = “ISSN: ${this.result.issn}”,
-              ${this.result.link ? `url = “${this.result.link}”,` : ''}
-              ${this.result.paginace_titulu ? `pages = “${this.result.paginace_titulu}”` : ''}
+        this.bibTex = `@article{https://digiarchiv.aiscr.cz/id/${this.result.ident_cely},
+              author = {${autor}},
+              title = {${this.result.nazev} ${this.result.podnazev ? this.result.podnazev : ''}},
+              journal = {${this.result.casopis_denik_nazev}},
+              year = {${this.result.rok_vydani_vzniku}},
+              ${this.result.issn ? `issn = {${this.result.issn}},` : ''}
+              ${this.result.link ? `url = {${this.result.link}},` : ''}
+              ${this.result.paginace_titulu ? `pages = {${this.result.paginace_titulu}},` : ''}
+              volume = {${this.result.casopis_rocnik}}
             }`;
         break;
       case 'článek v novinách':
-        this.bibTex = `@article{
-                https://digiarchiv.aiscr.cz/id/${this.result.ident_cely},
-                author = “${autor}”,
-                title = “${this.result.nazev} ${this.result.podnazev ? this.result.podnazev : ''} ”,
-                journal = “${this.result.casopis_denik_nazev} ”,
-                year = ${this.result.rok_vydani_vzniku},
-                volume = “${this.result.datum_rd}”,
-                ${this.result.link ? `url = “${this.result.link}”,` : ''}
-                ${this.result.paginace_titulu ? `pages = “${this.result.paginace_titulu}”` : ''}
+        this.bibTex = `@article{https://digiarchiv.aiscr.cz/id/${this.result.ident_cely},
+                author = {${autor}},
+                title = {${this.result.nazev} ${this.result.podnazev ? this.result.podnazev : ''}},
+                journal = {${this.result.casopis_denik_nazev}},
+                year = {${this.result.rok_vydani_vzniku}},
+                ${this.result.link ? `url = {${this.result.link}},` : ''}
+                ${this.result.paginace_titulu ? `pages = {${this.result.paginace_titulu}},` : ''}
+                volume = {${this.result.datum_rd}}
               }`;
         break;
       case 'nepublikovaná zpráva':
-        this.bibTex = `@unpublished{
-                  https://digiarchiv.aiscr.cz/id/${this.result.ident_cely},
-                  author = “${autor}”,
-                  title = “${this.result.oznaceni} ”,
-                  year = ${this.result.rok_vydani_vzniku},
-                  ${this.result.link ? `url = “${this.result.link}”,` : ''}
-                  note = “${this.result.organizace}”
+        this.bibTex = `@unpublished{https://digiarchiv.aiscr.cz/id/${this.result.ident_cely},
+                  author = {${autor}},
+                  title = {${this.result.oznaceni}},
+                  year = {{this.result.rok_vydani_vzniku}},
+                  ${this.result.link ? `url = {${this.result.link}},` : ''}
+                  publisher = {${this.result.organizace}}
                 }`;
         break;
     }
