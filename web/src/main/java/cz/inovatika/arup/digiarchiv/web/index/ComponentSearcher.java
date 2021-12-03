0ea96ea14bb4dca20f34786b443156351db3b22d
alberto <alberto.hernandez@inovatika.cz>
2021-12-03 14:02:32 +0100

    #128

---
+++ web/src/main/java/cz/inovatika/arup/digiarchiv/web/index/ComponentSearcher.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.inovatika.arup.digiarchiv.web.index;
+
+import javax.servlet.http.HttpServletRequest;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author alberto
+ */
+public interface EntitySearcher {
+  public JSONObject search(HttpServletRequest request);
+  public void getChilds(JSONObject jo, HttpSolrClient client, HttpServletRequest request);
+}
