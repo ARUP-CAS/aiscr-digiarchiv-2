7650ba089e8bb695fd934d39bb9aac79e37d3130
albertoh <alberto.hernandez@inovatika.cz>
2023-10-23 10:46:55 +0200

    #238 ano/ne

--- web/src/main/java/cz/inovatika/arup/digiarchiv/web/I18n.java
+++ web/src/main/java/cz/inovatika/arup/digiarchiv/web/I18n.java
@@ -82,7 +82,7 @@ public class I18n {
     // Organizace
     String field = "nazev"; 
     if ("en".equals(locale)) {
-      field += "_en";
+      field += "_zkraceny_en";
     }
     String urlOrg = opts.getString("solrhost", "http://localhost:8983/solr/")
             + "organizations/export?q=*:*&wt=json&sort=ident_cely%20asc&fl=ident_cely," + field;
