117c60272a5a1b33387eb0d5e77e7b835ac5719b
albertoh <alberto.hernandez@inovatika.cz>
2025-08-12 10:56:21 +0200

    #443 Workflow updates

--- web/src/main/ng/src/app/app.routes.ts
+++ web/src/main/ng/src/app/app.routes.ts
@@ -3,15 +3,23 @@ import { Home } from './pages/home/home';
 import { ResultsComponent } from './pages/results/results.component';
 import { StatsComponent } from './pages/stats/stats.component';
 import { MapViewContainerComponent } from './pages/map-view/map-view-container.component';
+import { DocumentComponent } from './pages/document/document.component';
+import { ExportMapaComponent } from './pages/export-mapa/export-mapa.component';
+import { ExportComponent } from './pages/export/export.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: Home },
     { path: 'results', component: ResultsComponent },
     { path: 'stats', component: StatsComponent },
-    { path: 'map', 
+    {
+        path: 'map',
         loadComponent: () => import('./pages/map-view/map-view-container.component').then(m => m.MapViewContainerComponent)
-    //    component: MapViewContainerComponent,  
-    }
+        //    component: MapViewContainerComponent,  
+    },
+    { path: 'id/:id', component: DocumentComponent },
+    { path: 'print/:id', component: DocumentComponent },
+    { path: 'export', component: ExportComponent },
+    { path: 'export-mapa', component: ExportMapaComponent },
 
 ];
