566eb3a5440d0a8569876912926c36dfa1034852
Petr Kudela <kudy.samba@gmail.com>
2025-10-17 14:02:24 +0200

    #443 - document dialog fix

--- web/src/main/ng/src/app/components/document-dialog/document-dialog.component.ts
+++ web/src/main/ng/src/app/components/document-dialog/document-dialog.component.ts
@@ -1,11 +1,12 @@
 
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, forwardRef } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogRef, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { TranslateModule } from '@ngx-translate/core';
 import { AppService } from '../../app.service';
+import { EntityContainer } from '../../entities/entity-container/entity-container';
 
 @Component({
   imports: [
@@ -13,7 +14,8 @@ import { AppService } from '../../app.service';
     MatDialogModule,
     MatIconModule,
     MatButtonModule,
-    MatTooltipModule
+    MatTooltipModule,
+    forwardRef(() => EntityContainer),
 ],
   selector: 'app-document-dialog',
   templateUrl: './document-dialog.component.html',
@@ -37,6 +39,10 @@ export class DocumentDialogComponent implements OnInit {
     this.service.getId(this.result.ident_cely).subscribe((res: any) => {
       this.result = res.response.docs[0];
     });
+  }
+
+  isResultEmpty(obj: any) {
+    return (!obj || (Object.keys(obj).length === 0));
   }
 
 }
