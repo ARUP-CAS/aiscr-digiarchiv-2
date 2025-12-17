587c804b4443eca2b98cd23f928d1f6532906079
albertoh <alberto.hernandez@inovatika.cz>
2025-12-17 11:16:58 +0100

    #534

--- web/src/main/java/cz/inovatika/arup/digiarchiv/web4/oai/OAIServlet.java
+++ web/src/main/java/cz/inovatika/arup/digiarchiv/web4/oai/OAIServlet.java
@@ -77,7 +77,7 @@ public class OAIServlet extends HttpServlet {
                 out.println(xml);
             } else {
                 String xml = OAIRequest.headerOAI() + OAIRequest.responseDateTag()
-                        + "<request>" + Options.getInstance().getJSONObject("OAI").getString("baseUrl") + "/" + version + "</request>"
+                        + "<request>" + Options.getInstance().getJSONObject("OAI").getString("baseUrl") + "/" + version + "/oai</request>"
                         + "<error code=\"badVerb\">verb is absent</error>"
                         + "</OAI-PMH>";
                 out.print(xml);
