45ee8602224e1a46cca8e0132f5aff0f27c5d683
albertoh <alberto.hernandez@inovatika.cz>
2026-01-20 13:22:35 +0100

    Security fixes

--- web/src/main/java/cz/inovatika/arup/digiarchiv/web4/fedora/FedoraUtils.java
+++ web/src/main/java/cz/inovatika/arup/digiarchiv/web4/fedora/FedoraUtils.java
@@ -12,13 +12,8 @@ import java.net.http.HttpResponse;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 import java.util.Base64;
-import java.util.logging.Level;
-import org.apache.solr.common.SolrInputDocument;
+import org.apache.commons.text.StringEscapeUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -46,7 +41,7 @@ public class FedoraUtils {
     //HttpClient client = HttpClient. newHttpClient();
     HttpRequest request = HttpRequest.newBuilder()
             .GET()
-            .uri(new URI(API_POINT + url)) 
+            .uri(new URI(API_POINT + StringEscapeUtils.escapeHtml4(url))) 
             .header("Authorization", auth_header())
             .header("Accept", "application/ld+json")
             .build();
@@ -60,7 +55,7 @@ public class FedoraUtils {
     //HttpClient client = HttpClient. newHttpClient();
     HttpRequest request = HttpRequest.newBuilder()
             .GET()
-            .uri(new URI(SEARCH_POINT + urlQuery))
+            .uri(new URI(SEARCH_POINT + StringEscapeUtils.escapeHtml4(urlQuery)))
             .header("Authorization", auth_header())
             //.header("Accept", "application/ld+json")
             .build();
