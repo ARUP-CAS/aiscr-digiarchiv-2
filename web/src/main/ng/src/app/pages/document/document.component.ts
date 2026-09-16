2bc56b5480e38923290b6bf02eb0b3454099963e
albertoh <alberto.hernandez@inovatika.cz>
2026-09-16 09:32:22 +0200

    #953

--- web/src/main/ng/src/app/pages/document/document.component.ts
+++ web/src/main/ng/src/app/pages/document/document.component.ts
@@ -80,7 +80,7 @@ export class DocumentComponent implements OnInit, AfterViewInit {
         this.state.imagesLoading = this.state.imagesLoaded < this.state.numImages;
         this.tryPrint();
       } else {
-        this.state.loading.set(true);
+        this.state.printing.set(true);
         this.service.print();
       }
     }, 2000);
